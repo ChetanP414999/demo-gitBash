dc9a10841d744f3cb29845d0c5818769d585d281
ChetanP414999 <chetanpatil6927@gmail.com>
2025-01-27 12:31:09 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,8 +3,9 @@ public class Student
 	public static void main (String args [])
 	{
 		System.out.println("Hello from local repo");
-	
-	
+		System.out.println("Hello from Central rep");
+		
+		
 	}
 
 
